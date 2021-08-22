c9b2ad5c6307f7174f249dcc3ee36ea1a6b0c9ff
OriAlves <orialves04@gmail.com>
2021-08-22 18:51:56 -0300

    añado package.json

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
 
-
+app.use('/static', express.static(__dirname + '/public'));
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
     });
