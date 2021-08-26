51aa659e93f9269f8eae5090bd525c33a65a1ee2
OriAlves <orialves04@gmail.com>
2021-08-26 18:52:43 -0300

    Añado avances en index

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
     });
